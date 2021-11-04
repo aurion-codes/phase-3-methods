0cc0051826917e0d62433a314a3603c54bfad057
aurionhoward <aurionhoward@gmail.com>
2021-11-04 10:29:43 -0400

    Completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+# Your code here!puts
+def greet_programmer
+    puts "Hello name!"
+end
+
+def greet_programmer(name)
+    puts "Hello #{name}"
+end
+
+def greet_with_default(name ="programmer")
+    puts "Hello #{name}!"
+end
+
+def add(num1, num2)
+     num1 + num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+
+    num / 2
+end
+    
